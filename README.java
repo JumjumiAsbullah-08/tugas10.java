3d10eafc73410e47eab7bf3bab727c86700ace41
Jumjumi Asbullah <76870885+JumjumiAsbullah-08@users.noreply.github.com>
2021-05-18 21:52:42 +0700

    Update README.java

--- README.java
+++ README.java
@@ -1,1 +1,23 @@
 # tugas10.java
+DOSEN PENGAMPU : MUHAMMAD DEDI IRAWAN, M.KOM
+==============================================
+package com.mycompany.dulla.dullah;
+
+public class abdul {
+
+    public static void main(String[] args) {
+     char A = 'A';
+    
+     System.out.println(" A = " + A);
+     A++;
+     System.out.println(" A = " + A);
+     A++;
+     System.out.println(" H = " + A);
+     A--;
+     System.out.println(" H = " + A);
+     
+     System.out.println(" Nama Teman Saya Adalah = Abdullah");
+     
+     System.out.println(" Nama Akhir Saya Adalah = Asbullah");
+    }
+}
